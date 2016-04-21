3a167f1ee7b67a09ba0883d14c87a56cdd985079
Marcus Uhlmann <art1_marcus@hotmail.com>
2016-04-20 21:42:33 -0300

    Enum implementado na classe Cliente

--- ImplementaAbstrato/src/br/univel/Cliente.java
+++ ImplementaAbstrato/src/br/univel/Cliente.java
@@ -7,4 +7,45 @@ public class Cliente {
 	private String end;
 	private String tel;
 	
+	public static void EC(EstadoCivil estado){
+		
+	}
+	
+	public static void main(String []args){
+		EC(EstadoCivil.C);
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEnd() {
+		return end;
+	}
+
+	public void setEnd(String end) {
+		this.end = end;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	
+	
 }
