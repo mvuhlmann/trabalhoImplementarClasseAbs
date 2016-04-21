ab25ab1df87b42d135da3ae011f40563bf5208a4
Marcus Uhlmann <art1_marcus@hotmail.com>
2016-04-20 22:07:30 -0300

    Atributos e metodos criados na annotation Tabela

--- ImplementaAbstrato/src/br/univel/Tabela.java
+++ ImplementaAbstrato/src/br/univel/Tabela.java
@@ -1,5 +1,14 @@
 package br.univel;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface Tabela {
+
+	String value() default "";
 
 }
