24eee4d948a4cef0f934c04cd07a854bac9d69d1
Marcus Uhlmann <art1_marcus@hotmail.com>
2016-04-20 22:27:38 -0300

    Metodo main e enum implementados na classe aplicacaodeteste

--- ImplementaAbstrato/src/br/univel/AplicacaoDeTeste.java
+++ ImplementaAbstrato/src/br/univel/AplicacaoDeTeste.java
@@ -1,5 +1,13 @@
 package br.univel;
 
 public class AplicacaoDeTeste {
+	
+	public static void EC(EstadoCivil estado){
+		
+	}
+	
+	public static void main(String []args){
+		EC(EstadoCivil.CASADO);
+	}
 
 }
