edce572f2aee027f1d6f03323ed1eae0c7e2b91a
Marcus Uhlmann <art1_marcus@hotmail.com>
2016-04-25 20:58:26 -0300

    PreparedStatement pronto para ser executado na classe ParteQuatro que extend de SqlGen

--- ImplementaAbstrato/src/br/univel/ParteQuatro.java
+++ ImplementaAbstrato/src/br/univel/ParteQuatro.java
@@ -1,6 +1,9 @@
 package br.univel;
 
+import java.lang.reflect.Field;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class ParteQuatro extends SqlGen{
 
@@ -46,4 +49,101 @@ public class ParteQuatro extends SqlGen{
 		return null;
 	}
 
+	private PreparedStatement getPreparedStatementForInset(Connection con, Object obj) {
+
+		Class<? extends Object> cl = obj.getClass();
+
+		StringBuilder sb = new StringBuilder();
+
+		// Declara��o da tabela.
+		{
+			String nomeTabela;
+			if (cl.isAnnotationPresent(Tabela.class)) {
+
+				Tabela anotacaoTabela = cl.getAnnotation(Tabela.class);
+				nomeTabela = anotacaoTabela.value();
+
+			} else {
+				nomeTabela = cl.getSimpleName().toUpperCase();
+
+			}
+			sb.append("INSERT INTO ").append(nomeTabela).append(" (");
+		}
+
+		Field[] atributos = cl.getDeclaredFields();
+
+		// Declara��o das colunas
+		{
+			for (int i = 0; i < atributos.length; i++) {
+
+				Field field = atributos[i];
+
+				String nomeColuna;
+
+				if (field.isAnnotationPresent(Coluna.class)) {
+					Coluna anotacaoColuna = field.getAnnotation(Coluna.class);
+
+					if (anotacaoColuna.nome().isEmpty()) {
+						nomeColuna = field.getName().toUpperCase();
+					} else {
+						nomeColuna = anotacaoColuna.nome();
+					}
+
+				} else {
+					nomeColuna = field.getName().toUpperCase();
+				}
+
+				if (i > 0) {
+					sb.append(", ");
+				}
+
+				sb.append(nomeColuna);
+			}
+		}
+
+		sb.append(") VALUES (");
+
+		for (int i = 0; i < atributos.length; i++) {
+			if (i > 0) {
+				sb.append(", ");
+			}
+			sb.append('?');
+		}
+		sb.append(')');
+
+		String strSql = sb.toString();
+		System.out.println("SQL GERADO: " + strSql);
+
+		PreparedStatement ps = null;
+		try {
+			ps = con.prepareStatement(strSql);
+
+			for (int i = 0; i < atributos.length; i++) {
+				Field field = atributos[i];
+
+				// importante n�o esquecer
+				field.setAccessible(true);
+
+				if (field.getType().equals(int.class)) {
+					ps.setInt(i + 1, field.getInt(obj));
+
+				} else if (field.getType().equals(String.class)) {
+					ps.setString(i + 1, String.valueOf(field.get(obj)));
+
+				} else {
+					throw new RuntimeException("Tipo n�o suportado, falta implementar.");
+
+				}
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+
+		return ps;
+	}
 }
