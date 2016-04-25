22d9bc3617fb6659287442dd6f525b21eb557419
Marcus Uhlmann <art1_marcus@hotmail.com>
2016-04-25 20:39:08 -0300

    Metodos da classe abstrata implementados

--- ImplementaAbstrato/src/br/univel/ParteQuatro.java
+++ ImplementaAbstrato/src/br/univel/ParteQuatro.java
@@ -1,5 +1,49 @@
 package br.univel;
 
-public class ParteQuatro {
+import java.sql.PreparedStatement;
+
+public class ParteQuatro extends SqlGen{
+
+	@Override
+	protected String getCreateTable(Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String getDropTable(Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected PreparedStatement getSqlInsert(Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected PreparedStatement getSqlSelectAll(Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected PreparedStatement getSqlSelectById(Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected PreparedStatement getSqlUpdateById(Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected PreparedStatement getSqlDeleteById(Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
