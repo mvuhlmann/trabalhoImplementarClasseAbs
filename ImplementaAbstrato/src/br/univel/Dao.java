eca3eaf126022578bd313dcc4785fe1ffae13941
Marcus Uhlmann <art1_marcus@hotmail.com>
2016-04-25 21:10:50 -0300

    Metodos criados na classe Dao

---
+++ ImplementaAbstrato/src/br/univel/Dao.java
@@ -0,0 +1,13 @@
+package br.univel;
+
+import java.awt.List;
+
+public interface Dao <T, K>{
+	
+		public void salvar(T t);
+		public T buscar(K k);
+		public void atualizar(T t);
+		public void excluir(K k);
+		public List listarTodos();
+		
+}
