ebcabacb300edc657389394c4fd528cdf4badd54
Marcus Uhlmann <art1_marcus@hotmail.com>
2016-04-25 21:20:00 -0300

    Classe parte cinco criada e implementando a interface Dao

---
+++ ImplementaAbstrato/src/br/univel/ParteCinco.java
@@ -0,0 +1,29 @@
+package br.univel;
+
+public class ParteCinco<T, K> implements Dao<T, K>{
+
+	@Override
+	public void salvar(T t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T buscar(K k) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void atualizar(T t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void excluir(K k) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
