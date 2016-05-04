ea52656423a445c4db81b1e228e1d2a311853053
Marcus Uhlmann <art1_marcus@hotmail.com>
2016-05-04 16:14:48 -0300

    Classe que implementa a interface Dao criada

---
+++ ImplementaAbstrato/src/br/univel/classes/FilhaDao.java
@@ -0,0 +1,39 @@
+package br.univel.classes;
+
+import java.util.List;
+
+import br.univel.interfaces.Dao;
+
+public class FilhaDao implements Dao{
+
+	@Override
+	public void salvar(Object t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object buscar(Object k) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void atualizar(Object t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void excluir(Object k) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List listarTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
