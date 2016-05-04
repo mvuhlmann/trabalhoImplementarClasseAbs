698ef8b1dadc08f45deece86a2487a7fd9ca1437
Marcus Uhlmann <art1_marcus@hotmail.com>
2016-05-04 20:44:14 -0300

    Classe que conecta ao H2 criada

--- ImplementaAbstrato/src/br/univel/classes/ConectaBanco.java
+++ ImplementaAbstrato/src/br/univel/classes/ConectaBanco.java
@@ -1,5 +1,25 @@
 package br.univel.classes;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConectaBanco {
 
+private Connection con = null;
+	
+	public Connection abrircon() throws SQLException {
+
+		String url = "";
+		String user = "";
+		String pass = "";
+		con = DriverManager.getConnection(url, user, pass);
+		
+		return con;
+		
+	}
+	
+	public void fecharConexao() throws SQLException{
+		con.close();
+	}
 }
