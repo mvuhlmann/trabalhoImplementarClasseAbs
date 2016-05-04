25092e7a8ac2ba13be2fc502bc81c56e319c51db
Marcus Uhlmann <art1_marcus@hotmail.com>
2016-05-04 16:03:34 -0300

    Classe que extend de SqlGen para implementar a mae abstrata

---
+++ ImplementaAbstrato/src/br/univel/classes/FilhaSqlGen.java
@@ -0,0 +1,51 @@
+package br.univel.classes;
+
+import java.sql.PreparedStatement;
+
+import br.univel.abstratas.SqlGen;
+
+public class FilhaSqlGen extends SqlGen{
+
+	@Override
+	protected String getCreateTable(Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String getDropTable(Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected PreparedStatement getSqlInsert(Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected PreparedStatement getSqlSelectAll(Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected PreparedStatement getSqlSelectById(Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected PreparedStatement getSqlUpdateById(Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected PreparedStatement getSqlDeleteById(Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
